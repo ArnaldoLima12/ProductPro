f0f5880aebb5137be5b7a36cfcd3b73defc5e7ce
ArnaldoLima12 <arnaldolima588@gmail.com>
2024-04-07 22:57:11 -0300

    c

--- backend/config/session.js
+++ backend/config/session.js
@@ -1,12 +1,12 @@
 const session = require('express-session');
 const MongoStore = require('connect-mongo'); 
 
-let options = {mongoUrl:'mongodb+srv://arnaldolima588:puiMkwO0voFh532Y@product.ksq95do.mongodb.net/?retryWrites=true&w=majority&appName=Product', dbName: 'productpro', autoRemove: 'native', collectionName: 'sessions'};
+let options = {mongoUrl:'mongodb+srv://arnaldolima588:puiMkwO0voFh532Y@product.ksq95do.mongodb.net/?retryWrites=true&w=majority&appName=Product', dbName: 'productpro', collectionName: 'sessions'};
 
 const initSession = app => {
     app.use(session({
         name: 'ProductPro',
-        secret: 'dajsfafyfa9jf9sasasyfa9hf9ashfa',
+        secret: 'dajsfafyfa9jf9sasasyfa9hf9adsadadadashfa',
         resave: false,
         saveUninitialized: false,
         store: MongoStore.create(options),
