ab17dfe07091cf36234e114eeff8c0c843769036
ArnaldoLima12 <arnaldolima588@gmail.com>
2024-04-07 22:36:01 -0300

    Sem

--- backend/config/session.js
+++ backend/config/session.js
@@ -1,10 +1,10 @@
 const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session); // Chame a função passando 'session' como argumento
-const store =  new MongoDBStore({
-    uri: 'mongodb+srv://arnaldolima588:puiMkwO0voFh532Y@product.ksq95do.mongodb.net/?retryWrites=true&w=majority&appName=Product',
-    databaseName: 'productpro',
-    collection: 'session'
-}, function(erro) {console.log(erro)});
+// const MongoDBStore = require('connect-mongodb-session')(session); // Chame a função passando 'session' como argumento
+// const store =  new MongoDBStore({
+//     uri: 'mongodb+srv://arnaldolima588:puiMkwO0voFh532Y@product.ksq95do.mongodb.net/?retryWrites=true&w=majority&appName=Product',
+//     databaseName: 'productpro',
+//     collection: 'session'
+// }, function(erro) {console.log(erro)});
 
 
 const initSession = app => {
@@ -13,7 +13,7 @@ const initSession = app => {
         secret: 'dajsfafyfa9jf9sasasyfa9hf9ashfa',
         resave: false,
         saveUninitialized: false,
-        store: store,
+        // store: store,
         cookie: {maxAge: 15 * 60 * 1000, secure: false}
     }));
 };
